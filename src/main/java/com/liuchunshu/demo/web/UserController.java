8dc134f9c85c232442e1ea135b2de12a72afcb7c
fei5841 <33828459+fei5841@users.noreply.github.com>
2018-04-18 09:22:49 +0800

    2018-04-18 09:22:45

--- src/main/java/com/liuchunshu/demo/web/UserController.java
+++ src/main/java/com/liuchunshu/demo/web/UserController.java
@@ -2,23 +2,43 @@ package com.liuchunshu.demo.web;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.liuchunshu.demo.entity.UserEntity;
 import com.liuchunshu.demo.mapper.test1.User1Mapper;
 import com.liuchunshu.demo.mapper.test2.User2Mapper;
+import com.liuchunshu.demo.util.JwtToken;
 
 @RestController
 public class UserController {
 
+	private Logger logger=LoggerFactory.getLogger(UserController.class);
+	
     @Autowired
     private User1Mapper user1Mapper;
 
 	@Autowired
 	private User2Mapper user2Mapper;
+	
+	@Value("${JWTToken}")
+	private String screat;
+	
+	@RequestMapping(value="/index",method=RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView model=new ModelAndView("index");
+		System.out.println("index request");
+//		String token=JwtToken.createToken(screat);
+		model.addObject("token", screat);
+		return model;
+	}
 	
 	@RequestMapping("/getUsers")
 	public List<UserEntity> getUsers() {
@@ -26,7 +46,7 @@ public class UserController {
 		return users;
 	}
 	
-    @RequestMapping("/getUser")
+    @RequestMapping(value="/getUser",method=RequestMethod.POST,produces="application/json;charset=utf-8")
     public UserEntity getUser(Long id) {
     	UserEntity user=user2Mapper.getOne(id);
         return user;
--- src/test/java/com/liuchunshu/demo/MybatisDatasourcesDemoApplicationTests.java
+++ src/test/java/com/liuchunshu/demo/MybatisDatasourcesDemoApplicationTests.java
@@ -11,7 +11,6 @@ public class MybatisDatasourcesDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		System.out.pringln("text");
 		System.out.println("test");
 	}
 
